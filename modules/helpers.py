1a6757e4614f7218340e0334861652954a7ca688
onordberg <OeysteinNordberg@gmail.com>
2020-07-09 13:44:19 +0200

    Full rework of tile generator

---
+++ modules/helpers.py
@@ -0,0 +1,15 @@
+def get_int_uid(meta, string_UIDs):
+    return meta.loc[string_UIDs]['int_uid'].tolist()
+
+def get_string_uid(meta, int_UIDs):
+    # Could probably be neater
+    # Accepts list of ints and single int
+    if type(int_UIDs) == int:
+        int_UIDs = [int_UIDs]
+    l = []
+    for int_UID in int_UIDs:
+        l.append(meta[meta['int_uid'] == int_UID].index.tolist()[0])
+    if len(l) == 1:
+        return l[0]
+    else:
+        return l
